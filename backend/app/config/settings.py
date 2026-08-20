5925cc81c35f16071e1dacdf2a5e71d294eedccb
Gojo-111 <199323123+Gojo-111@users.noreply.github.com>
2026-08-20 16:26:42 -0500

    Setup a test database
    The test suite was running against the same DB as
    the dev environnement which was causing some
    weird errors. That's what this commit solves!

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: str
+    test_database_url: str | None = None
+
     session_secret: str
     token_encryption_key: str
 
